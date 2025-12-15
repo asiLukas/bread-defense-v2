b014367bfc0b549583efcea57ece89bd4c5d0841
Lukáš Zagorský <lukas.zagorsky@seznam.cz>
2025-12-15 14:15:46 +0100

    add player with basic movement

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 
+from player import Player
+
 pygame.init()
-
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1280, 720))
 FPS = 60
-
 clock = pygame.time.Clock()
+
+player_group = pygame.sprite.GroupSingle()
+player = Player(640, 360, scale_factor=4)
+player_group.add(player)
 
 while True:
     for event in pygame.event.get():
@@ -13,7 +17,13 @@ while True:
             pygame.quit()
             raise SystemExit
 
-    screen.fill("purple")  
+    screen.fill("black")
+
+    player_group.update()
+    player_group.draw(screen)
+    
+    # debug collision
+    # pygame.draw.rect(screen, "red", player.rect, 2)
 
     pygame.display.flip()
     clock.tick(FPS)
